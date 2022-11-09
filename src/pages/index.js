05339c3b40524b5e0d9cc0c555b66edc1d6dda73
Kassian Wren <the@nodebotani.st>
2022-11-09 16:05:47 -0600

    Added Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import * as React from "react"
 import {graphql, Link} from "gatsby"
 
+import Layout from "../components/layout"
+
 const Index = ({data}) => {
     const monsters = data.allMonster.nodes
     console.log(monsters)
     return(
-        <>
+        <Layout>
             <h1 className="text-3xl font-bold underline">Bestiary</h1>
             <ul>
                 {monsters.map((monster) => <li><Link to={`/monsters/${monster.id}`}>{monster.name}</Link></li>)}
             </ul>
-        </>
+        </Layout>
     )
 }
 
--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 
+import Layout from "../components/layout"
+
 const TrackerPage = () => {
     return (
-        <>
+        <Layout>
             <h1>Combat Tracker</h1>
-        </>
+        </Layout>
     )
 }
 
