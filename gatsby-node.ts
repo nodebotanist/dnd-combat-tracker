7a22c452d234f2b7f40e2d3f4908093d89c6b92e
Kassian Wren <the@nodebotani.st>
2022-12-13 08:50:51 -0600

    Added data nodes back

---
+++ gatsby-node.ts
@@ -0,0 +1,32 @@
+import type { GatsbyNode } from "gatsby"
+
+import { request, gql } from "graphql-request"
+
+export const sourceNodes: GatsbyNode['sourceNodes'] = async ({ actions, createNode, createNodeId, createContentDigest }) => {
+    const query = gql`
+    query {
+        monsters (limit: 500) {
+            name
+            desc
+            challenge_rating
+            hit_points
+            image
+        }
+    }`
+
+    let monsters = await request('https://www.dnd5eapi.co/graphql', query)
+    monsters = monsters.monsters
+
+    monsters.forEach((monster) => {
+        actions.createNode({
+            // Data for the node.
+            ...monster,
+            // Required fields.
+            id: createNodeId(monster.name),
+            internal: {
+                type: `monster`,
+                contentDigest: createContentDigest(monster)
+            }
+        })
+    })
+}
